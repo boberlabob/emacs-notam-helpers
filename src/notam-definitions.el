9824c3da39a1140e6ad3b890952839e7205c5cfd
boberlabob <tobias.portmann@gmail.com>
2016-11-22 13:51:37 +0100

    Create notam-definitions.el

---
+++ src/notam-definitions.el
@@ -0,0 +1,7 @@
+;;;;;;;;
+;; Regex definitions
+;;;;;;;;
+
+(defconst re-notam-nr "[A-Z][0-9]\\{4\\}/[0-9]\\{2\\}" "A NOTAM number")
+(defconst re-notam-nr "[A-Z][0-9]\\{4\\}/[0-9]\\{2\\}" "A NOTAM number")
+
